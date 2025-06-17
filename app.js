556ff61814b974813aad7de615f23ef05fa4a3a0
Andrii Brelyk <andrii.brelyk@lobyco.com>
2025-06-17 20:10:30 +0300

    task pgsql

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { sequelize } from "./db/db.js";
+import dotenv from "dotenv";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -21,6 +23,25 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+dotenv.config();
+const { PORT = 3000 } = process.env;
+
+await sequelize
+    .authenticate()
+    .then(async () => {
+        console.log("✅ Database connection successful");
+
+        // 🔄 Синхронізація моделей з базою даних
+        await sequelize.sync({ alter: true });
+        console.log("📦 All models synchronized with the database");
+
+        // ▶️ Запуск сервера
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is running. Use our API on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`❌ Database connection failed: ${error.message}`);
+        process.exit(1);
+    });
+
